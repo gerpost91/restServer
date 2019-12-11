121f60187afa91770400acac9787d1dc3999e8a9
Gerardo Guevara <gerardoguevara800@gmail.com>
2019-12-11 11:59:35 -0600

    adding config

--- server/server.js
+++ server/server.js
@@ -1,9 +1,7 @@
-
 //requieres 
 const express = require('express')
 const app = express()
 // *****bodyParser* *****/´
-
 const bodyParser = require('body-parser')
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false })) 
@@ -11,16 +9,19 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 // ojo el body parser complementa express, antes eran el mismo paquete pqeo se separaron
 
+
 //********************/
 // *****CRUD  ******//
 //*****************/
 
-// get is mostly used to fetch data, it's by default used on browsers
+// GET is mostly used to fetch data, it's by default used on browsers
 app.get('/usuario', function (req, res) {
     res.json('Hello usuario you are using the get method')
 })
 
-// post is mostly used to create new data
+
+
+// POST is mostly used to create new data
 app.post('/usuario', function (req, res) {
 
     let body = req.body; // gracias al body parser
@@ -31,18 +32,13 @@ app.post('/usuario', function (req, res) {
             messages: 'name is necesary'
         })
 
-
     } else {
         res.json({ persona:body}) 
-    }
-
-       
-
-    
+    }    
 })
 
 
-// put is mostly used to update data as well path
+// PUT is mostly used to update data as well path
 // retorna la misma data que recibe
 app.put('/usuario/:id', function (req, res) {
 
@@ -53,11 +49,14 @@ app.put('/usuario/:id', function (req, res) {
     })
 })
 
-// currently it's no so normal delete data from a DB so the delete metho is commonly use to  change  the data
+// currently DELETE it's no so normal delete data from a DB so the delete metho is commonly use to  change  the data
 app.delete('/usuario', function (req, res) {
     res.json('Hello usuario you are using the delete method')
 })
 
+
+
+// simple status alert
 app.listen(3000, () => {
     console.log("Working on port 3000");
 }
