7345d573030b8ca6f176e799bd8a73a213a00176
Gerardo Guevara <gerardoguevara800@gmail.com>
2019-12-10 22:36:08 -0600

    adding crud methos

--- server/server.js
+++ server/server.js
@@ -1,11 +1,35 @@
 const express = require('express')
 const app = express()
- 
-app.get('/', function (req, res) {
-  res.json('Hello World')
+
+
+// get is mostly used to fetch data, it's by default used on browsers
+app.get('/usuario', function (req, res) {
+    res.json('Hello usuario you are using the get method')
 })
- 
-app.listen(3000,()=>
-{console.log("Working on port 3000");
+
+// post is mostly used to create new data
+app.post('/usuario', function (req, res) {
+    res.json('Hello usuario you are using the post method')
+})
+
+
+// put is mostly used to update data as well path
+// retorna la misma data que recibe
+app.put('/usuario/:id', function (req, res) {
+
+    let id = req.params.id;
+    res.json({
+        id, 
+
+    })
+})
+
+// currently it's no so normal delete data from a DB so the delete metho is commonly use to  change  the data
+app.delete('/usuario', function (req, res) {
+    res.json('Hello usuario you are using the delete method')
+})
+
+app.listen(3000, () => {
+    console.log("Working on port 3000");
 }
 )
